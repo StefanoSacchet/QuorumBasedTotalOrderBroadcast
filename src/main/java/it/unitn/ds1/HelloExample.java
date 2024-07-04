12ecd6c7da26dd2b3b7e115d0633691d5ee004f3
StefanoSacchet <sacchet13@gmail.com>
2024-07-04 09:14:04 +0200

    First commit

---
+++ src/main/java/it/unitn/ds1/HelloExample.java
@@ -0,0 +1,12 @@
+package it.unitn.ds1;
+
+import java.io.IOException;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
+public class HelloExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
